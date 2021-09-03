05f22353dd5bce48a9cca94ab468a1642daf933b
Admin <Admin@LAPTOP-OJAUB9J8>
2021-09-03 17:56:01 +0200

    Dodaj pliki projektów.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using stackoverflow_tags.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace stackoverflow_tags.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Home_Index()
+        {
+            decimal count_sum = 0;
+            RootObject tag = new RootObject();
+            List<RootObject> tagList = new List<RootObject>();
+            using (var httpClient = new HttpClient())
+            {
+
+
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        var bytes = await httpClient.GetByteArrayAsync("https://api.stackexchange.com/2.3/tags?page=" + i + "&pagesize=100&order=desc&sort=popular&site=stackoverflow");
+                        var decompressedJson = new StreamReader(new GZipStream(new MemoryStream(bytes), CompressionMode.Decompress)).ReadToEnd();
+
+                        tag = JsonConvert.DeserializeObject<RootObject>(decompressedJson);
+                    
+                        tagList.Add(tag);
+                    }
+                    foreach (var items in tagList)
+                    {
+                        foreach (var item in items.items)
+                        {
+                            count_sum = count_sum + item.count;
+                        }
+                    }
+
+                    foreach (var items in tagList)
+                    {
+                        foreach (var item in items.items)
+                        {
+                        item.procent = ((item.count * 100) / count_sum);
+                        }
+                    }
+
+
+
+
+            }
+           
+            return View(tagList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/tagsClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stackoverflow_tags.Models
+{
+    public class items
+    {
+        public List<collectives> collectives { get; set; }
+        public string has_synonyms { get; set; }
+        public string is_moderator_only { get; set; }
+        public string is_required { get; set; }
+        [Display(Name ="Ilość użyć (popularność)")]
+        public int count { get; set; }
+        [Display(Name ="Tags")]
+        public string name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00000000000000000000}", ApplyFormatInEditMode = true)]
+        [Display(Name ="Procent w populacji")]
+        public decimal procent { get; set; }
+    }
+
+    public class external_links
+    {
+        public string type { get; set; }
+        public string link { get; set; }
+    }
+
+    public class tags
+    {
+        public string tag { get; set; }
+    }
+
+    public class collectives
+    {
+        public string[] tags { get; set; }
+        public List<external_links> external_links { get; set; }
+        public string description { get; set; }
+        public string link { get; set; }
+        public string name { get; set; }
+        public string slug { get; set; }
+    }
+
+    public class RootObject : items
+    {
+        public List<items> items { get; set; }
+        public string has_more { get; set; }
+        public string quota_max { get; set; }
+        public string quota_remaining { get; set; }
+    }
+}
